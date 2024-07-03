fa367f1e88a54e3f5cffe1c0f1429f972ce531db
Kim minho <klaus9267@gmail.com>
2024-07-03 20:42:57 +0900

    init

---
+++ src/main/java/kimandhong/oxox/OxoxApplication.java
@@ -0,0 +1,13 @@
+package kimandhong.oxox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kimandhong/oxox/OxoxApplicationTests.java
@@ -0,0 +1,13 @@
+package kimandhong.oxox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
