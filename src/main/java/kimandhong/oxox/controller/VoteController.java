7db2a87c711978285aff2fb83665b0e63be82b52
Kim minho <klaus9267@gmail.com>
2024-07-12 13:00:46 +0900

    Feat: 투표하기에서 isYes = null이면 투표 취소로 변경

--- src/main/java/kimandhong/oxox/controller/VoteController.java
+++ src/main/java/kimandhong/oxox/controller/VoteController.java
@@ -1,11 +1,13 @@
 package kimandhong.oxox.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
-import kimandhong.oxox.common.swagger.SwaggerNoContent;
 import kimandhong.oxox.common.swagger.SwaggerOK;
 import kimandhong.oxox.service.VoteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/votes")
@@ -15,15 +17,9 @@ public class VoteController {
   private final VoteService voteService;
 
   @PostMapping
-  @SwaggerOK(summary = "투표하기",description = "isYes = null 이면 투표 취소로 변경 예정")
+  @SwaggerOK(summary = "투표하기", description = "isYes = null 이면 투표 취소")
   public void vote(@RequestParam final Long postId,
-                   @RequestParam final boolean isYes) {
+                   @RequestParam(required = false) final Boolean isYes) {
     voteService.vote(postId, isYes);
-  }
-
-  @DeleteMapping("{postId}")
-  @SwaggerNoContent(summary = "투표취소")
-  public void deleteVote(@PathVariable("postId") final Long postId) {
-    voteService.deleteVote(postId);
   }
 }
--- src/main/java/kimandhong/oxox/service/VoteService.java
+++ src/main/java/kimandhong/oxox/service/VoteService.java
@@ -19,28 +19,24 @@ public class VoteService {
   private final SecurityUtil securityUtil;
 
   @Transactional
-  public void vote(final Long postId, final boolean isYes) {
+  public void vote(final Long postId, final Boolean isYes) {
     final User user = securityUtil.getCurrentUser();
     final Post post = postService.findById(postId);
 
     voteRepository.findByUserIdAndPostId(user.getId(), postId)
-        .ifPresentOrElse(vote -> vote.updateIsYes(isYes),
-            () -> {
-              final Vote vote = Vote.from(isYes, user, post);
-              voteRepository.save(vote);
-            }
-        );
-  }
-
-  @Transactional
-  public void deleteVote(final Long postId) {
-    final User user = securityUtil.getCurrentUser();
-
-    voteRepository.findByUserIdAndPostId(user.getId(), postId).ifPresentOrElse(
-        voteRepository::delete,
-        () -> {
-          throw new NotFoundException(ErrorCode.NOT_FOUND_VOTE);
-        }
-    );
+        .ifPresentOrElse(vote -> {
+          if (isYes != null) {
+            vote.updateIsYes(isYes);
+          } else {
+            voteRepository.delete(vote);
+          }
+        }, () -> {
+          if (isYes != null) {
+            final Vote vote = Vote.from(isYes, user, post);
+            voteRepository.save(vote);
+          } else {
+            throw new NotFoundException(ErrorCode.NOT_FOUND_VOTE);
+          }
+        });
   }
 }
