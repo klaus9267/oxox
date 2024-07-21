e5283b339c92ea8313393b78dbba889d20f3fb11
klaus9267 <klaus9267@gmail.com>
2024-07-21 17:18:04 +0900

    Fix: cors 해결

--- src/main/java/kimandhong/oxox/config/WebConfig.java
+++ src/main/java/kimandhong/oxox/config/WebConfig.java
@@ -9,9 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost")
-        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+        .allowedOriginPatterns("/*")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         .allowedHeaders("*")
-        .allowCredentials(true);
+        .allowCredentials(true)
+        .maxAge(3600);
   }
 }
