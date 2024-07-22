99ab5be48528049aeb2b2f003b3612e516ef1001
Kim minho <klaus9267@gmail.com>
2024-07-22 12:07:19 +0900

    Test: dto직접 반환으로 인한 repo test code 삭제

--- src/test/java/kimandhong/oxox/repository/custom/PostCustomRepositoryTest.java
+++ src/test/java/kimandhong/oxox/repository/custom/PostCustomRepositoryTest.java
@@ -3,8 +3,9 @@ package kimandhong.oxox.repository.custom;
 import jakarta.persistence.EntityManager;
 import kimandhong.oxox.common.DataInitializer;
 import kimandhong.oxox.config.TestQueryDslConfig;
-import kimandhong.oxox.controller.param.SortType;
+import kimandhong.oxox.controller.param.PostCondition;
 import kimandhong.oxox.domain.Post;
+import kimandhong.oxox.dto.post.PostDto;
 import kimandhong.oxox.repository.PostRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -36,35 +37,5 @@ class PostCustomRepositoryTest {
   void init() {
     em = testEntityManager.getEntityManager();
     dataInitializer.init();
-  }
-
-  @Test
-  public void 게시글_목록_조회_BEST_REACTIONS() {
-    dataInitializer.init();
-    List<Post> posts = postCustomRepository.findAllSorted(SortType.BEST_REACTION);
-
-    List<Integer> reactionCounts = posts.stream()
-        .map(post -> post.getComments().stream().map(
-                    comment -> comment.getEmojiCounts().values().stream().mapToInt(i -> i).sum()
-                ).mapToInt(i -> i)
-                .sum()
-        ).toList();
-
-    for (int i = 0; i < 9; i++) {
-      assertThat(reactionCounts.get(i)).isGreaterThanOrEqualTo(reactionCounts.get(i + 1));
-    }
-  }
-
-  @Test
-  public void 게시글_목록_조회_POPULARITY() {
-    List<Post> posts = postCustomRepository.findAllSorted(SortType.POPULARITY);
-
-    List<Integer> voteCounts = posts.stream()
-        .map(post -> post.getVotes().size()
-        ).toList();
-
-    for (int i = 0; i < 9; i++) {
-      assertThat(voteCounts.get(i)).isGreaterThanOrEqualTo(voteCounts.get(i + 1));
-    }
   }
 }
