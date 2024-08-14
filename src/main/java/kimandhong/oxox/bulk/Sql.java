39225f6641c1a8aeb066a974ac030f366c304347
Kim minho <klaus9267@gmail.com>
2024-08-15 02:23:55 +0900

    fix/#68: 댓글 bulk api sql 수정

--- src/main/java/kimandhong/oxox/bulk/Sql.java
+++ src/main/java/kimandhong/oxox/bulk/Sql.java
@@ -10,11 +10,11 @@ public enum Sql {
   INSERT_PROFILE("INSERT INTO profiles (nickname, sequence, user_id) values (?, ?, ?)"),
   INSERT_POST("INSERT INTO posts (title, content, user_id, is_done,created_at) values (?, ?, ?, ?, ?)"),
   INSERT_VOTE("INSERT INTO votes (is_yes, user_id, post_id) values (?, ?, ?)"),
-  INSERT_COMMENT("INSERT INTO votes (is_yes, user_id, post_id) values (?, ?, ?)"),
+  INSERT_COMMENT("INSERT INTO comments (content, user_id, post_id) values (?, ?, ?)"),
   INSERT_REACTION("INSERT INTO reactions (emoji, user_id, comment_id) values (?, ?, ?)"),
 
-  UPDATE_PROFILE( "UPDATE profiles SET sequence = ? where id = ?"),
-  UPDATE_POST(  "UPDATE posts SET is_done = true where id = ?"),
+  UPDATE_PROFILE("UPDATE profiles SET sequence = ? where id = ?"),
+  UPDATE_POST("UPDATE posts SET is_done = true where id = ?"),
 
   DELETE_VOTE("DELETE FROM votes"),
   DELETE_REACTION("DELETE FROM reactions"),
@@ -22,7 +22,7 @@ public enum Sql {
   DELETE_COMMENT("DELETE FROM comments"),
   DELETE_POST("DELETE FROM posts"),
   DELETE_PROFILE("DELETE FROM profiles"),
-  DELETE_USER( "DELETE FROM users"),
+  DELETE_USER("DELETE FROM users"),
 
   KEY_0("SET FOREIGN_KEY_CHECKS = 0"),
   KEY_1("SET FOREIGN_KEY_CHECKS = 1"),
